3963857c51cd9bb6ddd78f5518bdf8e88597ad16
Sashok9203 <kovalchuk.olex@gmail.com>
2024-02-09 15:16:34 +0200

    Fix user birthdate

--- ASP.NET_OLX/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ASP.NET_OLX/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -136,13 +136,12 @@ namespace ASP.NET_OLX.Areas.Identity.Pages.Account
                 user.Name = Input.Name;
                 user.Surname = Input.Surname;
                 var result = await _userManager.CreateAsync(user, Input.Password);
-               // _userManager.AddToRoleAsync(user, "User");
-
+                
 				if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
-                    var userId = await _userManager.GetUserIdAsync(user);
+					await _userManager.AddToRoleAsync(user, "User");
+					var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
--- DataAccess/DefaultUsers.cs
+++ DataAccess/DefaultUsers.cs
@@ -1,12 +1,6 @@
 ﻿using DataAccess.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataAccess
 {
@@ -15,15 +9,15 @@ namespace DataAccess
 		public static void Initialize(ModelBuilder modelBuilder)
 		{
 			string ADMIN_ROLE_ID = "15b04d9eff654d8d966a172db59e2722";
-			string USER_ROLE_ID="59139483f3d1417db1efee50d14b6a7f";
-			string USER1_ID = "f66e492517d7414495e988c4c50fd107";
+			string USER_ROLE_ID = "59139483f3d1417db1efee50d14b6a7f";
+
+			string ADMIN_ID = "f66e492517d7414495e988c4c50fd107";
 			string USER2_ID = "d1901b2435594da2a255db13fc57509b";
 			string USER3_ID = "c86dc56aedf549f6afe5ceb4d414ebf1";
 			string USER4_ID = "028582c83a914a45b330b5234f4131fb";
 			string USER5_ID = "eb05f9548a2c4cf8adcc2be7305fc732";
            
 
-			//seed admin role
 			modelBuilder.Entity<IdentityRole>().HasData(
 				new IdentityRole
 			    {
@@ -42,16 +36,15 @@ namespace DataAccess
 				}
 			);
 
-			CreateUser(USER1_ID, "Admin@gmail.com",true,"Петро","Левак", ADMIN_ROLE_ID,"Admin_1",modelBuilder);
-			CreateUser(USER2_ID, "User1@gmail.com", true, "Iван", "Калита", USER_ROLE_ID, "User_1", modelBuilder);
-			CreateUser(USER3_ID, "User2@gmail.com", true, "Петро", "Дякуленко", USER_ROLE_ID, "User_2", modelBuilder);
-			CreateUser(USER4_ID, "User3@gmail.com", true, "Олег", "Панасенко", USER_ROLE_ID, "User_3", modelBuilder);
-			CreateUser(USER5_ID, "User4@gmail.com", true, "Тимофій", "Гнатенко", USER_ROLE_ID, "User_4", modelBuilder);
+			CreateUser(new DateTime(1998,1,12),ADMIN_ID, "Admin@gmail.com",true,"Петро","Левак", ADMIN_ROLE_ID,"Admin_1",modelBuilder);
+			CreateUser(new DateTime(1988, 10, 14), USER2_ID, "User1@gmail.com", true, "Iван", "Калита", USER_ROLE_ID, "User_1", modelBuilder);
+			CreateUser(new DateTime(2000, 11, 5), USER3_ID, "User2@gmail.com", true, "Петро", "Дякуленко", USER_ROLE_ID, "User_2", modelBuilder);
+			CreateUser(new DateTime(1999, 7, 7), USER4_ID, "User3@gmail.com", true, "Олег", "Панасенко", USER_ROLE_ID, "User_3", modelBuilder);
+			CreateUser(new DateTime(2001, 6, 9), USER5_ID, "User4@gmail.com", true, "Тимофій", "Гнатенко", USER_ROLE_ID, "User_4", modelBuilder);
 		}
 
-		private static void CreateUser(string userId,string email,bool emailConfirmed,string name,string surname,string roleId,string password, ModelBuilder modelBuilder)
+		private static void CreateUser(DateTime birthdate,string userId,string email,bool emailConfirmed,string name,string surname,string roleId,string password, ModelBuilder modelBuilder)
 		{
-			//create user
 			var appUser = new User
 			{
 				Id = userId,
@@ -60,9 +53,10 @@ namespace DataAccess
 				Name = name,
 				Surname = surname,
 				UserName = email,
-				NormalizedUserName = email.ToUpper()
+				NormalizedUserName = email.ToUpper(),
+				Birthdate = birthdate
 			};
-			PasswordHasher<User> ph = new PasswordHasher<User>();
+			PasswordHasher<User> ph = new();
 			appUser.PasswordHash = ph.HashPassword(appUser, password);
 			modelBuilder.Entity<User>().HasData(appUser);
 			modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
